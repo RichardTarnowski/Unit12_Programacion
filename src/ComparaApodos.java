30507027a4560274f3b9efc3844cc9c639615be4
Richard Tarnowski <alutar8264@ieselcaminas.org>
2023-05-29 12:27:04 +0200

    Solucionado un pequeño error en la interfaz comparable de apodos.

--- src/ComparaApodos.java
+++ src/ComparaApodos.java
@@ -6,6 +6,6 @@ public class ComparaApodos implements Comparator <Socio>  {
 
     @Override
     public int compare(Socio o1, Socio o2) {
-        return o1.getNombre().compareTo(o2.getNombre());
+        return o1.getApodo().compareToIgnoreCase(o2.getApodo());
     }
 }
